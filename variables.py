9120435e72882bdf836fb3f20a55c854a058e62c
Deepayan Chakravarti <deepayan.chakravarti@gmail.com>
2026-04-15 14:19:32 +0530

    Added a file

---
+++ variables.py
@@ -0,0 +1,7 @@
+character_name = "John Doe"
+age = 70
+is_male = True
+print("There once was a man named " + character_name)
+print("He was " + str(age) + " years old.")
+print("He really liked the name " + character_name)
+print("But didn't like being " + str(age) + ".")
