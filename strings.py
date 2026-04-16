9383fb7bc2921d3ec6e02cbc915f63b41cec5ef6
Deepayan Chakravarti <deepayan.chakravarti@gmail.com>
2026-04-16 23:13:26 +0530

    Added a file

---
+++ strings.py
@@ -0,0 +1,11 @@
+str = "Deepayan"
+print(str)
+print(str.upper()) #DEEPAYAN
+print(str.lower()) #deepayan
+print(str.isupper()) #False
+print(str.upper().isupper()) #True
+print(len(str)) #8
+print(str[0]) #D
+print(str[1]) #e
+print(str.index("p")) #2
+print(str.replace("D", "J")) #Jeepayan
