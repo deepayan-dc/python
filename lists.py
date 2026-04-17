d34e2c67cdc7f83388252a92b981b0b365777d0b
Deepayan Chakravarti <deepayan.chakravarti@gmail.com>
2026-04-17 17:10:32 +0530

    Added files

---
+++ lists.py
@@ -0,0 +1,7 @@
+subjects = ["java", "dbms", "toc", "pslp", "cns"]
+print(subjects)
+print(subjects[0])
+print(subjects[1])
+print(subjects[2])
+print(subjects[-1]) # negative indexing (access from the back)
+print(subjects[1:4]) # slicing (access a range of elements)
