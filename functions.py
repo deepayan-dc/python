37658bf31d23b60d89d2b284727a55e1d88bb649
Deepayan Chakravarti <deepayan.chakravarti@gmail.com>
2026-04-18 14:55:53 +0530

    Added files

---
+++ functions.py
@@ -0,0 +1,10 @@
+def greet(name, age):
+    print("Hello " + name + ", you are " + str(age) + " years old.")
+
+greet("Deepayan", 20)
+
+def sum (a, b):
+    return a + b
+
+result = sum(5, 10)
+print(result)
