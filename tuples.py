520c53b7b069fa08a66e0e8a3746a643de5f5694
Deepayan Chakravarti <deepayan.chakravarti@gmail.com>
2026-04-18 14:45:00 +0530

    Added files

---
+++ tuples.py
@@ -0,0 +1,5 @@
+coordinates = (5, 10) # creating a tuple
+print(coordinates) 
+print(coordinates[0]) # accessing elements of a tuple
+print(coordinates[1]) # tuples are immutable (cannot be changed)
+# coordinates[0] = 15 # this will raise an error
