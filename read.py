1237ccea6c7ed284bd9cbaca9499ce05ca7e68c8
Deepayan Chakravarti <deepayan.chakravarti@gmail.com>
2026-05-03 15:47:28 +0530

    added and updated files

--- read.py
+++ read.py
@@ -1,4 +1,8 @@
 file = open("text.txt", "r")
-
-print(file.readline())
+for line in file.readlines():
+    print(line)
 file.close()
+file.read() #reads the entire file as a string
+file.readline() #reads the next line of the file
+file.readable() #returns true if the file can be read
+file.closed #returns true if the file is closed
